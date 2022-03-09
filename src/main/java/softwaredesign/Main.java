37485535873f7243d23f9b6cfb72c7bbe6c1b9ac
Jay Doerga <doerga1@gmail.com>
2022-03-09 21:37:59 +0100

    exit and welcome

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         CommandHandler commandHandler = new CommandHandler();
+        System.out.println("Welcome to the Squid Game Control Room");
         while (true) {
             System.out.print("> ");
             Scanner scanner = new Scanner(System.in);
@@ -14,6 +15,9 @@ public class Main {
                 commandHandler.setGameState(command);
             } else if(command.contains("GET GAMESTATE")) {
                 commandHandler.getGameState(command);
+            } else if(command.contains("EXIT")) {
+                System.out.println("Exited the Squid Game Control Room");
+                break;
             }
         }
     }

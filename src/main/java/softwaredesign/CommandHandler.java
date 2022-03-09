475b13c010ce04e641e187165b917cd945e08d82
Jay Doerga <doerga1@gmail.com>
2022-03-09 21:34:40 +0100

    gamestate change

---
+++ src/main/java/softwaredesign/CommandHandler.java
@@ -0,0 +1,39 @@
+package softwaredesign;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandHandler {
+    Map<String, String> games;
+
+    public CommandHandler() {
+        games = new HashMap<>();
+    }
+
+    protected void setGameState(String command) {
+        String[] commandSplitted = command.split(" ");
+        if (commandSplitted.length > 4) {
+            System.out.println("Invalid command");
+            return;
+        }
+        String gameName = commandSplitted[2];
+        String state = commandSplitted[3];
+        games.put(gameName, state);
+        System.out.println("Set the game " + gameName + " to " + state);
+    }
+
+    protected void getGameState(String command) {
+        String[] commandSplitted = command.split(" ");
+        if (commandSplitted.length > 3) {
+            System.out.println("Invalid command");
+            return;
+        }
+        String gameName = commandSplitted[2];
+        String state = games.get(gameName);
+        if (state == null) {
+            System.out.println("The game " + gameName + " does not exist!");
+            return;
+        }
+        System.out.println("The gamestate of " + gameName + " is " + state);
+    }
+}
--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -1,23 +1,20 @@
 package softwaredesign;
 
-import java.io.*;
+import java.util.Scanner;
 
-import java.net.*;
-import java.util.Arrays;
-import io.netty.*
 public class Main {
-    public static void main (String[] args) throws IOException {
-        Socket socket = new Socket();
-        InetAddress inetAddress=InetAddress.getByName("localhost");
-        System.out.println(inetAddress.toString());
-        //the port should be greater or equal to 0, else it will throw an error
-        int port=3333;
-        //calling the constructor of the SocketAddress class
-        SocketAddress socketAddress=new InetSocketAddress(inetAddress, port);
-        //binding the  socket with the inetAddress and port number
-        //connect() method connects the specified socket to the server
-        socket.connect(socketAddress);
-        System.out.println("Inet address: "+socket.getInetAddress());
-        System.out.println("Port number: "+socket.getLocalPort());
+    public static void main (String[] args) {
+        CommandHandler commandHandler = new CommandHandler();
+        while (true) {
+            System.out.print("> ");
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+            System.out.println(command);
+            if(command.contains("SET GAMESTATE")) {
+                commandHandler.setGameState(command);
+            } else if(command.contains("GET GAMESTATE")) {
+                commandHandler.getGameState(command);
+            }
+        }
     }
 }

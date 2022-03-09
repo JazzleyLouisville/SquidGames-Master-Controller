b357ce92c61d064fa03cebc85b946603f064648d
Jay Doerga <doerga1@gmail.com>
2022-03-09 20:18:58 +0100

    netty

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -1,37 +1,23 @@
 package softwaredesign;
 
 import java.io.*;
-import io.socket.client.IO;
-import io.socket.client.Socket;
 
+import java.net.*;
 import java.util.Arrays;
+import io.netty.*
 public class Main {
     public static void main (String[] args) throws IOException {
-        String url = "http://localhost:9999";
-        try {
-            IO.Options options = new IO.Options();
-            options.transports = new String[]{"websocket"};
-            // Number of failed retries
-            options.reconnectionAttempts = 10;
-            // Time interval for failed reconnection
-            options.reconnectionDelay = 1000;
-            // Connection timeout (ms)
-            options.timeout = 500;
-            final Socket socket = IO.socket(url, options);
-            // Listening for custom MSG events
-            Socket.on ("msg", "objects-> System.out.println" ("client: received msg->" + Arrays.toString (objects)));
-            // Listening for custom subscription events
-            Socket.on ("sub", objects - > System.out.println ("client:"+ "successful subscription, feedback - >" + Arrays.toString (objects)));
-            socket.on(Socket.EVENT_CONNECT, objects -> {
-                Socket. emit ("sub", "I'm a subscriber");
-                System. out. println ("client:"+ "successful connection");
-            });
-            Socket.on (Socket.EVENT_CONNECTING, objects-> System.out.println ("client:"+"in connection");
-            Socket.on (Socket.EVENT_CONNECT_TIMEOUT, objects-> System.out.println ("client:"+"connection timeout");
-            Socket.on (Socket.EVENT_CONNECT_ERROR, objects-> System.out.println ("client:"+"connection failure");
-            socket.connect();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        Socket socket = new Socket();
+        InetAddress inetAddress=InetAddress.getByName("localhost");
+        System.out.println(inetAddress.toString());
+        //the port should be greater or equal to 0, else it will throw an error
+        int port=3333;
+        //calling the constructor of the SocketAddress class
+        SocketAddress socketAddress=new InetSocketAddress(inetAddress, port);
+        //binding the  socket with the inetAddress and port number
+        //connect() method connects the specified socket to the server
+        socket.connect(socketAddress);
+        System.out.println("Inet address: "+socket.getInetAddress());
+        System.out.println("Port number: "+socket.getLocalPort());
     }
 }

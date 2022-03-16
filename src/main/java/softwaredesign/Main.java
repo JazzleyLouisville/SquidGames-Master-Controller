d3c338fb9ec3242b6040c21e7c0e2cc482b3b470
Zefan Morsen <z.b.morsen@student.vu.nl>
2022-03-16 20:08:01 +0100

    gui

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -1,27 +1,48 @@
 package softwaredesign;
 
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+//import javafx.*;
 
-public class Main {
+public class Main extends Application {
     public static void main (String[] args) {
-        CommandHandler commandHandler = new CommandHandler();
-        System.out.println("Welcome to the Squid Game Control Room");
-        while (true) {
-            System.out.print("> ");
-            Scanner scanner = new Scanner(System.in);
-            String command = scanner.nextLine();
-            if(command.contains("SET GAMESTATE")) {
-                commandHandler.setGameState(command);
-            } else if(command.contains("GET GAMESTATE")) {
-                commandHandler.getGameState(command);
-            } else if(command.contains("SET GAMESEQUENCE")) {
-                commandHandler.setGameSequence(command);
-            }else if(command.contains("GET GAMESEQUENCE")) {
-                commandHandler.getGameSequence();
-            } else if(command.contains("EXIT")) {
-                System.out.println("Exited the Squid Game Control Room");
-                break;
-            }
-        }
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Test");
+
+        StackPane layout = new StackPane();
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+//        CommandHandler commandHandler = new CommandHandler();
+//        System.out.println("Welcome to the Squid Game Control Room");
+//        while (true) {
+//            System.out.print("> ");
+//            Scanner scanner = new Scanner(System.in);
+//            String command = scanner.nextLine();
+//            if(command.contains("SET GAMESTATE")) {
+//                commandHandler.setGameState(command);
+//            } else if(command.contains("GET GAMESTATE")) {
+//                commandHandler.getGameState(command);
+//            } else if(command.contains("SET GAMESEQUENCE")) {
+//                commandHandler.setGameSequence(command);
+//            }else if(command.contains("GET GAMESEQUENCE")) {
+//                commandHandler.getGameSequence();
+//            } else if(command.contains("EXIT")) {
+//                System.out.println("Exited the Squid Game Control Room");
+//                break;
+//            }
+//        }
+//    }
+
 }

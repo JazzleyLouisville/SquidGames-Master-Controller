c79d48a1171f794fec1891d93c5c99fc6159e763
Jazzley Louisville <j.j.j.louisville@student.vu.nl>
2022-03-18 22:42:17 +0100

    gradle runs

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 //import javafx.*;
 
 public class Main extends Application {
+
+    Button button;
+//    Button button_user;
     public static void main (String[] args) {
         launch(args);
     }
@@ -18,11 +22,21 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Test");
 
-        StackPane layout = new StackPane();
+        button = new Button();
+        button.setText("Master");
+//        button_user = new Button();
+//        button_user.setText("User");
 
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+//        layout.getChildren().add(button_user);
         Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
     }
 //        CommandHandler commandHandler = new CommandHandler();
 //        System.out.println("Welcome to the Squid Game Control Room");

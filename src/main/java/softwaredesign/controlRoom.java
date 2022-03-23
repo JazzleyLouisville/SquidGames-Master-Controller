4de4bcba4bcd247f9f63ef294f4aa44723558fb1
jazz <j.j.j.louisville@student.vu.nl>
2022-03-23 23:06:44 +0100

    functionally working

--- src/main/java/softwaredesign/controlRoom.java
+++ src/main/java/softwaredesign/controlRoom.java
@@ -1,12 +1,57 @@
 package softwaredesign;
 
-import javafx.event.ActionEvent;
-import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.image.ImageView;
 
 public class controlRoom {
 
-    public void logOut (ActionEvent event) throws IOException {
-        Main m = new Main();
-        m.screenChange("src/main/java/softwaredesign/startScreen.fxml");
-    }
+    @FXML
+    private Tab InvitePlayersTab;
+
+    @FXML
+    private ListView<?> LstVwPlayers;
+
+
+    @FXML
+    private Label InviteAck;
+
+    @FXML
+    private Tab CurrentPlayersTab;
+
+    @FXML
+    private Label CenterLabel;
+
+    @FXML
+    private Label RightTopLabel;
+
+    @FXML
+    private Label RightBottomLabel;
+
+    @FXML
+    private Label LeftTopLabel;
+
+    @FXML
+    private Label LeftBottomLabel;
+
+    @FXML
+    private ImageView KillImg;
+
+    @FXML
+    private ImageView MsgImg;
+
+    @FXML
+    private Tab GameSequenceTab;
+
+    @FXML
+    private Tab CurrentGameTab;
+
+//    @FXML
+//    void controlRoom(ActionEvent event) {
+//
+//    }
+
 }

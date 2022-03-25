5ce8b20f380450172d30fc56e9f7b69735705c6c
jazz <j.j.j.louisville@student.vu.nl>
2022-03-25 21:12:26 +0100

    l

--- src/main/java/softwaredesign/devController.java
+++ src/main/java/softwaredesign/devController.java
@@ -1,4 +1,28 @@
 package softwaredesign;
 
-public class devLogin {
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class devController {
+
+    @FXML
+    private TextField gameNameLbl;
+
+    @FXML
+    private TextField roundLbl;
+
+    @FXML
+    private TextField winPerLbl;
+
+    @FXML
+    private Button devBtn;
+
+    @FXML
+    void devSend(ActionEvent event) {
+
+    }
+
 }
